c3497574c9f14f8bcd2e8cf762e0e53904e2559f
dustpancake <fergusbkr@gmail.com>
2020-10-23 11:58:06 +0200

    updated install to be self contained

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from jupyter_client.kernelspec import install_kernel_spec
+
+
+class KernelInstall(install):
+    def run(self):
+        install.run(self)
+        install_kernel_spec("kernel", kernel_name="Cadabra2")
+
 
 setup(
     name="cadabra2-jupyter",
@@ -6,5 +15,6 @@ setup(
     author="Fergus Baker",
     version="0.1.0",
     packages=find_packages(exclude=["client_server"]),
+    cmdclass={"install": KernelInstall},
     zip_safe=False,
 )
