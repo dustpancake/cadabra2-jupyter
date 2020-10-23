b580e8c959ad32df875529e42c013a8c1f8bc3e0
dustpancake <fergusbkr@gmail.com>
2020-10-23 09:47:14 +0200

    reformat with black

--- cadabra2jupyter/utils.py
+++ cadabra2jupyter/utils.py
@@ -1,12 +1,11 @@
 from io import BytesIO
 import urllib, base64
 
+
 def _plt_to_png(fig):
     """Return a base64-encoded PNG from a
     matplotlib figure."""
     imgdata = BytesIO()
-    fig.savefig(imgdata, format='png')
+    fig.savefig(imgdata, format="png")
     imgdata.seek(0)
-    return urllib.parse.quote(
-        base64.b64encode(imgdata.getvalue())
-    )
+    return urllib.parse.quote(base64.b64encode(imgdata.getvalue()))
